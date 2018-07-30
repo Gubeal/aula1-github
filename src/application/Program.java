ca692a68758f3dc16eb56ef34ea373d722be2a26
Gubeal <celsogubeal@gmail.com>
2018-07-30 02:17:26 -0300

    Exercicio de composicao resolvido

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,11 +6,15 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Client;
 import entities.Comment;
 import entities.Department;
 import entities.HourContract;
 import entities.Order;
+import entities.Order1;
+import entities.OrderItem;
 import entities.Post;
+import entities.Product;
 import entities.Worker;
 import entities.enums.OrderStatus;
 import entities.enums.WorkerLevel;
@@ -18,32 +22,64 @@ import entities.enums.WorkerLevel;
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		
+		
+		System.out.println("Enter client data:");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Email: ");
+		String email = sc.nextLine();
+		System.out.print("Birth date (DD/MM/YYYY): ");
+		Date birthDate = sdf.parse(sc.nextLine());
+		
+		System.out.println("Enter order data:");
+		System.out.print("Status: ");
+		String status = sc.nextLine();
+		Date moment = new Date();
+		Order1 order = new Order1(moment, OrderStatus.valueOf(status), new Client(name, email, birthDate) ); 
+		
+		System.out.print("How many items to this order? ");
+		int n = sc.nextInt();
+		
+		for (int i=1; i<=n; i++) {
+			System.out.println("Enter #" + i + " item data: ");
+			System.out.print("Product name: ");
+			sc.nextLine();
+			String productName = sc.nextLine();
+			System.out.print("Product price: ");
+			Double productPrice = sc.nextDouble();
+			System.out.print("Quantity: ");
+			int productQuantity = sc.nextInt();
+			order.addItem(new OrderItem(productQuantity, productPrice, new Product(productName, productPrice)));
+		}
+		
+		System.out.println();
+		System.out.println(order);
+		sc.close();
+	}
+
+	public static void aprendendoStringBuilder() throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
 		Comment c1 = new Comment("Have a nice trip!");
 		Comment c2 = new Comment("Wow that's awesome!");
-		Post p1 = new Post(
-				sdf.parse("21/06/2018 13:05:44"),
-				"Traveling to the New Zealand",
-				"I'm going to visit this wonderful country!",
-				12);
+		Post p1 = new Post(sdf.parse("21/06/2018 13:05:44"), "Traveling to the New Zealand",
+				"I'm going to visit this wonderful country!", 12);
 		p1.addComment(c1);
 		p1.addComment(c2);
-		
+
 		Comment c3 = new Comment("Good night");
 		Comment c4 = new Comment("May the Force be with you");
-		Post p2 = new Post(
-				sdf.parse("28/07/2018 23:14:19"),
-				"Good night guys",
-				"See you tomorrow",
-				5);
+		Post p2 = new Post(sdf.parse("28/07/2018 23:14:19"), "Good night guys", "See you tomorrow", 5);
 		p2.addComment(c3);
 		p2.addComment(c4);
-		
+
 		System.out.println(p1);
-		
+
 		System.out.println(p2);
-		
 	}
 
 	public static void resolvido1() throws ParseException {
